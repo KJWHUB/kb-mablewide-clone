209f602967af070fef0fbbe6cbe2d6ce4ee27f5d
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-07-02 23:58:26 +0900

    "@nuxtjs/color-mode", "nuxt-svgo" add

--- components/default-layout/AppSidebar.vue
+++ components/default-layout/AppSidebar.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts"></script>
 
 <template>
-  <aside>
-    <h1>App Sidebar</h1>
+  <aside class="main-layout-sidebar">
+    <!-- home -->
+    <!-- like -->
+    <!-- trading -->
+    <!-- prime club -->
+    <!-- my assets  -->
+
+    <!-- settings -->
+    <!-- dark mode -->
+    <!-- setting -->
+
+    <!-- menu expand -->
   </aside>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-layout-sidebar {
+  display: flex;
+  flex-direction: column;
+  background: var(--bg_type16);
+  padding: 1rem 0 4rem;
+  height: 100%;
+  width: 8rem;
+  position: fixed;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"]
-})
+  modules: ["@nuxt/eslint", "@nuxtjs/color-mode", "nuxt-svgo"],
+  colorMode: {
+    preference: "system",
+    fallback: "dark",
+  },
+  svgo: {
+    autoImportPath: "./assets/icons/",
+  },
+  css: ["~/assets/css/main.css"],
+});
