a5b276beabbfa6bc7d72a9bbee491c44237e553e
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-07-21 19:07:48 +0900

    레이아웃 재구성

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,30 @@
+<script setup lang="ts">
+const headerRef = ref<HTMLElement | null>(null);
+const headerHeight = ref(0);
+
+const borderHeight = 4;
+const calcHeaderHeight = computed(() => headerHeight.value + borderHeight);
+
+onMounted(() => {
+  headerHeight.value = headerRef.value?.clientHeight || 0;
+});
+</script>
+
 <template>
   <div class="main-layout">
     <!-- sidebar -->
     <DefaultLayoutAppSidebar />
     <div style="margin-left: 8rem; flex: 1">
       <!-- header -->
-      <DefaultLayoutAppHeader />
-      <main class="content">
+      <div ref="headerRef" class="header-wrap">
+        <DefaultLayoutAppHeader />
+      </div>
+      <main
+        class="content"
+        :style="{
+          marginTop: `${calcHeaderHeight}px`,
+        }"
+      >
         <slot />
       </main>
     </div>
@@ -23,11 +42,21 @@ $content-border: 4px solid $border-color;
   display: flex;
   overflow: hidden;
 
+  .header-wrap {
+    position: fixed;
+    top: 0;
+    left: 8rem;
+    width: calc(100% - 8rem);
+    z-index: 100;
+
+    border-bottom: $content-border;
+  }
+
   main.content {
     width: 100%;
     min-height: 100vh;
+
     border-left: $content-border;
-    border-top: $content-border;
   }
 }
 </style>
