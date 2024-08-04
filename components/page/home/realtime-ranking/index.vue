acc0e50a71b727ae00f08b7abcfa564363cf7a4c
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-08-04 23:19:09 +0900

    stock list props 로 전환

--- components/page/home/realtime-ranking/index.vue
+++ components/page/home/realtime-ranking/index.vue
@@ -1,5 +1,43 @@
 <script setup lang="ts">
+import type { TableData } from "../stock/list/index.vue";
+import { getPriceDiffType } from "../stock/list/Item.vue";
+
 const title = "실시간 랭킹" as const;
+
+const { data } = useFetch("/api/rank", {
+  method: "GET",
+});
+
+const tableDataComputed = computed<TableData[]>(() => {
+  if (!data.value) {
+    return [];
+  }
+
+  const processData = data.value.map((table) => ({
+    id: table.SORT_CD,
+    title: table.TITLE,
+    data: table.OUT2.map((item) => {
+      const rank = Number(item.rnk);
+      const imgUrl = `https://cdn.kbsec.com/logo/KR_${item.isCd}.png`;
+      const currentPrice = Number(item.nowPrc);
+      const priceDiff = Number(item.bdyCmpr);
+      const priceDiffRate = Number(item.upDwnRP2.trim());
+      const priceDiffType = getPriceDiffType(priceDiffRate);
+      return {
+        id: item.isCd,
+        rank,
+        imgUrl,
+        name: item.isNm,
+        currentPrice,
+        priceDiff,
+        priceDiffRate,
+        priceDiffType,
+      };
+    }),
+  }));
+
+  return processData;
+});
 </script>
 
 <template>
@@ -8,7 +46,7 @@ const title = "실시간 랭킹" as const;
     <TitleHomeSection :title="title" right-arrow />
 
     <!-- list -->
-    <PageHomeStockList />
+    <PageHomeStockList :table-data="tableDataComputed" />
   </div>
 </template>
 
--- components/page/home/stock/list/index.vue
+++ components/page/home/stock/list/index.vue
@@ -1,53 +1,20 @@
 <script setup lang="ts">
-import Item, { getPriceDiffType, type StockItem } from "./Item.vue";
+import Item, { type StockItem } from "./Item.vue";
 
-const { data } = useFetch("/api/rank", {
-  method: "GET",
-});
-
-console.log("data:::", data);
-
-type TableData = {
+export type TableData = {
   id: string;
   title: string;
   data: StockItem[];
 };
 
-const tableDataComputed = computed<TableData[]>(() => {
-  if (!data.value) {
-    return [];
-  }
-
-  const processData = data.value.map((table) => ({
-    id: table.SORT_CD,
-    title: table.TITLE,
-    data: table.OUT2.map((item) => {
-      const rank = Number(item.rnk);
-      const imgUrl = `https://cdn.kbsec.com/logo/KR_${item.isCd}.png`;
-      const currentPrice = Number(item.nowPrc);
-      const priceDiff = Number(item.bdyCmpr);
-      const priceDiffRate = Number(item.upDwnRP2.trim());
-      const priceDiffType = getPriceDiffType(priceDiffRate);
-      return {
-        id: item.isCd,
-        rank,
-        imgUrl,
-        name: item.isNm,
-        currentPrice,
-        priceDiff,
-        priceDiffRate,
-        priceDiffType,
-      };
-    }),
-  }));
-
-  return processData;
-});
+const props = defineProps<{
+  tableData: TableData[];
+}>();
 </script>
 
 <template>
   <div class="stock-list-wrap home-list">
-    <div v-for="table in tableDataComputed" :key="table.id" class="stock-list">
+    <div v-for="table in props.tableData" :key="table.id" class="stock-list">
       <!-- title -->
       <TitleHomeSection :title="table.title" right-arrow size="small" />
 
